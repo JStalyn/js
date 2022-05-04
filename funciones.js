281a041a638d8f034a465048f89c2e9aa5501c44
Stalyn <estalinxk3gmail.com>
2022-05-04 13:23:24 -0500

    jv

---
+++ funciones.js
@@ -0,0 +1,21 @@
+//funcion en forma de declaración 
+function miFuncion() {
+    return 3;
+}
+
+//función en forma de expresi´´on
+var miFuncion = function(a, b){
+    return a + b
+}
+
+miFuncion()
+
+function daludarEstudiante(estudiante) {
+    console.log(estudiante)
+}
+
+function suma(a,b) {
+   
+    return a + b
+}
+suma(1,2)
